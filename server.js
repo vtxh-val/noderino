f458b0a84113f97f256395b99abd8489db6b9ea5
InstructorDiogo <diogo.barros@ironhack.com>
2025-04-01 13:59:10 -0300

    creating number env feature

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const app = require("express")()
 const desiredPath = process.env.DESIRED_PATH || "/diogo"
 const port = process.env.PORT || 8080
+const number = process.env.NUMBER || 0
 
 app.get(desiredPath, (req, res) => {
-    res.send(`Hello from ${desiredPath}`)
+    res.send(`Hello from ${desiredPath} number ${number}!`)
 })
 
 app.listen(port, () => {
